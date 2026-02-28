4376cadaac47e211dc3536778a76e89fbee17567
3yakhansa <3yanisa@gmail.com>
2026-03-01 06:33:35 +0800

    Add project files.

---
+++ POSTTEST_1/POSTTEST_1/Form1.Designer.vb
@@ -0,0 +1,123 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        lblIPSemester = New Label()
+        txtIPSemester = New TextBox()
+        btnTambah = New Button()
+        btnReset = New Button()
+        lblIPKumulatif = New Label()
+        txtIPKumulatif = New TextBox()
+        lblStatus = New Label()
+        SuspendLayout()
+        ' 
+        ' lblIPSemester
+        ' 
+        lblIPSemester.AutoSize = True
+        lblIPSemester.Location = New Point(235, 117)
+        lblIPSemester.Name = "lblIPSemester"
+        lblIPSemester.Size = New Size(106, 21)
+        lblIPSemester.TabIndex = 0
+        lblIPSemester.Text = "IP Semester"
+        ' 
+        ' txtIPSemester
+        ' 
+        txtIPSemester.ForeColor = SystemColors.Menu
+        txtIPSemester.Location = New Point(362, 117)
+        txtIPSemester.Name = "txtIPSemester"
+        txtIPSemester.Size = New Size(253, 28)
+        txtIPSemester.TabIndex = 1
+        ' 
+        ' btnTambah
+        ' 
+        btnTambah.Location = New Point(362, 159)
+        btnTambah.Name = "btnTambah"
+        btnTambah.Size = New Size(123, 29)
+        btnTambah.TabIndex = 2
+        btnTambah.Text = "Tambah"
+        btnTambah.UseVisualStyleBackColor = True
+        ' 
+        ' btnReset
+        ' 
+        btnReset.Location = New Point(492, 159)
+        btnReset.Name = "btnReset"
+        btnReset.Size = New Size(123, 29)
+        btnReset.TabIndex = 3
+        btnReset.Text = "Reset"
+        btnReset.UseVisualStyleBackColor = True
+        ' 
+        ' lblIPKumulatif
+        ' 
+        lblIPKumulatif.AutoSize = True
+        lblIPKumulatif.Location = New Point(235, 198)
+        lblIPKumulatif.Name = "lblIPKumulatif"
+        lblIPKumulatif.Size = New Size(114, 21)
+        lblIPKumulatif.TabIndex = 4
+        lblIPKumulatif.Text = "IP Kumulatif"
+        ' 
+        ' txtIPKumulatif
+        ' 
+        txtIPKumulatif.Location = New Point(362, 197)
+        txtIPKumulatif.Name = "txtIPKumulatif"
+        txtIPKumulatif.Size = New Size(253, 28)
+        txtIPKumulatif.TabIndex = 5
+        ' 
+        ' lblStatus
+        ' 
+        lblStatus.AutoSize = True
+        lblStatus.Location = New Point(362, 236)
+        lblStatus.Name = "lblStatus"
+        lblStatus.Size = New Size(16, 21)
+        lblStatus.TabIndex = 6
+        lblStatus.Text = "-"
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(11F, 21F)
+        AutoScaleMode = AutoScaleMode.Font
+        BackColor = SystemColors.GradientInactiveCaption
+        ClientSize = New Size(880, 378)
+        Controls.Add(lblStatus)
+        Controls.Add(txtIPKumulatif)
+        Controls.Add(lblIPKumulatif)
+        Controls.Add(btnReset)
+        Controls.Add(btnTambah)
+        Controls.Add(txtIPSemester)
+        Controls.Add(lblIPSemester)
+        Font = New Font("Baskerville Old Face", 9F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        ForeColor = Color.MidnightBlue
+        Name = "Form1"
+        Text = "Kalkulator IP"
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents lblIPSemester As Label
+    Friend WithEvents txtIPSemester As TextBox
+    Friend WithEvents btnTambah As Button
+    Friend WithEvents btnReset As Button
+    Friend WithEvents lblIPKumulatif As Label
+    Friend WithEvents txtIPKumulatif As TextBox
+    Friend WithEvents lblStatus As Label
+
+End Class
---
+++ POSTTEST_1/POSTTEST_1/Form1.vb
@@ -0,0 +1,57 @@
+﻿Public Class Form1
+    Private totalIPSemester As Double = 0
+    Private jumlahSemester As Integer = 0
+
+    Private Sub btnTambah_click(sender As Object, e As EventArgs) Handles btnTambah.Click
+        Dim ipSemester As Double
+        If Double.TryParse(txtIPSemester.Text, ipSemester) Then
+            If ipSemester < 0 Or ipSemester > 4 Then
+                MessageBox.Show("Nilai IP Semester harus antara 0 dan 4.")
+                Return
+            End If
+
+            totalIPSemester += ipSemester
+            jumlahSemester += 1
+
+            Dim ipKumulatif As Double = totalIPSemester / jumlahSemester
+            txtIPKumulatif.Text = ipKumulatif.ToString("F2")
+
+            If ipKumulatif >= 3.5 Then
+                lblStatus.Text = "Cum Laude"
+            ElseIf ipKumulatif >= 3.0 Then
+                lblStatus.Text = "Sangat Memuaskan"
+            ElseIf ipKumulatif >= 2.5 Then
+                lblStatus.Text = "Memuaskan"
+            Else
+                lblStatus.Text = "Tidak Memuaskan"
+            End If
+        End If
+    End Sub
+
+    Private Sub btnReset_Click(sender As Object, e As EventArgs) Handles btnReset.Click
+        totalIPSemester = 0
+        jumlahSemester = 0
+        txtIPKumulatif.Text = ""
+        lblStatus.Text = ""
+        txtIPSemester.Text = ""
+    End Sub
+    Private Sub lblIPSemester_Click(sender As Object, e As EventArgs) Handles lblIPSemester.Click
+
+    End Sub
+
+    Private Sub txtIPSemester_TextChanged(sender As Object, e As EventArgs) Handles txtIPSemester.TextChanged, txtIPSemester.TextChanged
+
+    End Sub
+
+    Private Sub txtIPSemester_KeyPr(sender As Object, e As EventArgs)
+
+    End Sub
+
+    Private Sub txtIPSemester_KeyPress(sender As Object, e As KeyPressEventArgs) Handles txtIPSemester.KeyPress
+
+    End Sub
+
+    Private Sub txtIPKumulatif_KeyPress(sender As Object, e As KeyPressEventArgs) Handles txtIPKumulatif.KeyPress
+
+    End Sub
+End Class
